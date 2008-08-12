ee3eae2d710fd968ab8532fafeab73aa2e1a4da6
bmacgregor <bmacgregor>
2008-08-12 16:20:30 +0000

    Committing changes to Simple Wire Protocol that use alternative delimiters.

--- src/franz/wire/resultreader.py
+++ src/franz/wire/resultreader.py
@@ -26,45 +26,87 @@ from franz.wire.jdbctuples import JDBCTuples
 
 import codecs
 
-class StringsDictionary:
+def bytes_find(bytes, target, start=None):
     """
+    Like 'find', but for bytes instead of characters.  Assumes
+    that 'target' is a single character wide.
+    Appears to run FASTER than the build-in 'find'!!!
+    """
+    i = start or 0;
+    target = ord(target)
+    end = len(bytes)
+    while i < end:
+        if ord(bytes[i]) == target:
+            return i
+        i += 1
+    return -1
+
+
+NEW_NAMESPACE_ENTRY = u'!"'
+NEW_LANGUAGE_ENTRY = u'!#'
+NEW_DATATYPE_ENTRY = u'!$'
+BARE_LITERAL_ENTRY = u'"!'
+
+NAMESPACE_ENTRY_TYPE = 11
+DATATYPE_ENTRY_TYPE = 12
+LANGUAGE_ENTRY_TYPE = 13
+
+
+class CompressionDictionary(dict):
+    """
+    Records pairs consisting of a term type and usually, a string representing
+    an uncompressed namespace, datatype or language.
     """
     def __init__(self):
-        self.namespace_dict = {}
-        self.localname_dict = {}
-        self.literal_dict = {}
-        
-    def decode_namespace(self, ns):
-        """
-        Return the namespace (including trailing delimiter) for
-        the two-character encoding 'ns'.
-        """
-        namespace = self.namespace_dict.get(ns)
-        return namespace or "www.nasa.gov/miasma#"
+        self[NEW_NAMESPACE_ENTRY] = (NAMESPACE_ENTRY_TYPE, None)
+        self[NEW_LANGUAGE_ENTRY] = (DATATYPE_ENTRY_TYPE, None)
+        self[NEW_DATATYPE_ENTRY] = (LANGUAGE_ENTRY_TYPE, None)    
+        self[BARE_LITERAL_ENTRY] = (LITERAL_TYPE, None)            
+         
+#    def decode_namespace(self, ns):
+#        """
+#        Return the namespace (including trailing delimiter) for
+#        the two-character encoding 'ns'.
+#        """
+#        namespace = self.namespace_dict.get(ns)
+#        return namespace or "www.nasa.gov/miasma#"
+#
+#    def decode_localname(self, lcl):
+#        """
+#        Return the local name corresponding to the encoding 'lcl'
+#        """
+#        name = self.localname_dict.get(lcl)
+#        return name
+#    
+#    def decode_literal(self, lit):
+#        """
+#        Return the literal value corresponding to the encoding 'lit'.
+#        """
+#        lv = self.literal_dict.get(lit)
+#        return lv
 
-    def decode_localname(self, lcl):
-        """
-        Return the local name corresponding to the encoding 'lcl'
-        """
-        name = self.localname_dict.get(lcl)
-        return name
-    
-    def decode_literal(self, lit):
-        """
-        Return the literal value corresponding to the encoding 'lit'.
-        """
-        lv = self.literal_dict.get(lit)
-        return lv
-    
-    def add_namespace(self, ns, namespace):
-        self.namespace_dict[ns] = namespace
         
-    def add_localname(self, nm, localname):
-        self.localname_dict[nm] = localname
-        
-    def add_literal(self, lt, literal):
-        self.literal_dict[lt] = literal
-        
+    def process_dictionary_field(self, entry_type, string_field):
+        """
+        Decode 'string_field' and do what it says.
+        """
+        #print "PROCESS DICTIONARY FIELD", entry_type, string_field
+        key = string_field[0:2]
+        value = string_field[2:]
+        if NAMESPACE_ENTRY_TYPE == entry_type:
+            self[key] = (URI_TYPE, value)
+        elif DATATYPE_ENTRY_TYPE == entry_type:
+            self[key] = (TYPED_LITERAL_TYPE, value)
+        elif LANGUAGE_ENTRY_TYPE == entry_type:
+            self[key] = (LANGUAGE_LITERAL_TYPE, value)
+        else:
+            print "ERROR: Unrecognized dictionary code for string '%s'" % string_field
+
+URI_TYPE = 1
+BLANK_NODE_TYPE = 2
+LITERAL_TYPE = 3
+TYPED_LITERAL_TYPE = 4
+LANGUAGE_LITERAL_TYPE = 5
 
 class StringsTerm:
     """
@@ -72,10 +114,6 @@ class StringsTerm:
     from a strings message.  They can be combined, supplemented with
     dictionary lookup, to yield a complete resource or literal.
     """
-    URI_TYPE = 0
-    LITERAL_TYPE = 1
-    TYPED_LITERAL_TYPE = 2
-    LANGUAGE_LITERAL_TYPE = 3
     def __init__(self, dictionary):
         self.term_type = -1
         self.encoded_namespace = None
@@ -86,15 +124,17 @@ class StringsTerm:
         self.literal_value = None
         self.encoded_literal_value = None
         self.encoded_datatype = None
+        self.datatype = None
+        self.encoded_language = None
         self.language = None
         self.dictionary = dictionary
         
-    def getNamespace(self):
-        ns = self.namespace
-        if ns: return ns
-        ns = self.dictionary.decode_namespace(self.encoded_namespace)
-        self.namespace = ns
-        return ns
+#    def getNamespace(self):
+#        ns = self.namespace
+#        if ns: return ns
+#        ns = self.dictionary.decode_namespace(self.encoded_namespace)
+#        self.namespace = ns
+#        return ns
     
     def getLocalname(self):
         ln = self.localname
@@ -103,10 +143,10 @@ class StringsTerm:
         
     def getString(self):
         type = self.term_type
-        if type == StringsTerm.URI_TYPE:
+        if type == URI_TYPE:
             uri = self.uri
             if uri: return uri
-            ns = self.getNamespace()
+            ns = self.namespace
             ln = self.getLocalname()
             ## TEMPORARY TO HELP DIAGNOSE BUG:
             if not ln:
@@ -115,28 +155,31 @@ class StringsTerm:
             uri = ns + ln
             self.uri = uri
             return uri                
-        elif type == StringsTerm.LITERAL_TYPE:
+        elif type == LITERAL_TYPE:
             lit = self.literal_value
             if lit: return lit
             lit = self.dictionary.decode_literal(self.encoded_literal_value)
             self.literal_value = lit
             return lit
-        elif type == StringsTerm.TYPED_LITERAL_TYPE:
-            pass
-        elif type == StringsTerm.LANGUAGE_LITERAL_TYPE:
-            pass        
+        elif type == TYPED_LITERAL_TYPE:
+            lit = '"%s"^^<%s>' % (self.literal_value, self.datatype)
+            return lit
+        elif type == LANGUAGE_LITERAL_TYPE:
+            return '"%s"@%s' % (self.literal_value, self.language)
 
     def __str__(self):
-        if self.term_type == StringsTerm.URI_TYPE: return '|URI|' + self.getString()
+        if self.term_type == URI_TYPE: return '|URI|' + self.getString()
         else: return '|LIT|' + self.getString()
         
     def __repr__(self):
         return self.__str__()
         
-FIELD_DELIMITER = u'\uC0C1' 
-FIELD_DELIMITER = u'``'
-RECORD_DELIMITER = u'\uFEFF'
-RECORD_DELIMITER = u'\177\177'
+C0_FIELD_DELIMITER = u'\xC0' 
+#FIELD_DELIMITER = u'``'
+FIELD_DELIMITER = u'\010'
+C1_RECORD_DELIMITER = u'\xC1'
+#RECORD_DELIMITER = u'\177\177'
+RECORD_DELIMITER = u'\007'
 FIELD_DELIMITER_LENGTH = len(FIELD_DELIMITER)
 RECORD_DELIMITER_LENGTH = len(RECORD_DELIMITER)
 
@@ -150,7 +193,7 @@ class ResultReader:
         self.row_terms = []
         self.term_count = -1
         self.is_exhausted = False
-        self.dictionary = StringsDictionary()
+        self.dictionary = CompressionDictionary()
         self.socket_is_exhausted = False
         ## read in the first chunk of data
         self.read_chunk()
@@ -191,12 +234,12 @@ class ResultReader:
             self.strings_data = None
             self.socket_is_exhausted = True
             return
-        if not isinstance(bytes, unicode):
-            bytes = unicode(bytes, 'utf-8')
+#        if not isinstance(bytes, unicode):
+#            bytes = unicode(bytes, 'utf-8')
         self.strings_data = bytes
 #        if len(bytes) < 8192:
 #            print "READ CHUNK ", len(bytes)
-        
+    
     def get_straddle_string(self, partial_row):
         """
         'partial_row' needs to be concatenated with the beginning
@@ -211,7 +254,7 @@ class ResultReader:
             if not nextStrings:
                 return straddleString
             previousStraddleLen = len(straddleString)
-            nextStringPos = nextStrings.find(RECORD_DELIMITER)
+            nextStringPos = bytes_find(nextStrings, RECORD_DELIMITER)
             if nextStringPos >= 0:
                 ## tricky: include the delimiter so that 'straddlePos' is guaranteed to succeed:
                 straddleString = straddleString + nextStrings[0:nextStringPos + RECORD_DELIMITER_LENGTH]
@@ -219,7 +262,7 @@ class ResultReader:
                 nextStringPos = len(nextStrings)
                 straddleString = straddleString + nextStrings
             ## need to catch the case when the RECORD_DELIMITER spans the chunk:
-            straddlePos = straddleString.find(RECORD_DELIMITER)
+            straddlePos = bytes_find(straddleString, RECORD_DELIMITER)
             if straddlePos > 0:
                 if straddlePos - previousStraddleLen  < nextStringPos:
                     ## tricky: it must be the case that the delimiter was split between
@@ -234,16 +277,15 @@ class ResultReader:
     def get_strings_row(self):
         """
         Return a string that contains all of the substrings for the next
-        row.  Return None, if there is no next row.  If a buffer boundary
-        is encountered, handle it transparently.
+        row, starting at position 'self.cursor'.  Return None, if there is 
+        no next row.  If a buffer boundary is encountered, handle it transparently.
         """
-        #if self.is_exhausted: return None
         strings = self.strings_data
         index = self.cursor
         if index >= len(strings):
             pos = -1
         else:
-            pos = strings.find(RECORD_DELIMITER, index)
+            pos = bytes_find(strings, RECORD_DELIMITER, index)
         if pos >= 0:
             self.cursor = pos + RECORD_DELIMITER_LENGTH
             return strings[index:pos]
@@ -270,26 +312,41 @@ class ResultReader:
         rowCursor = 0
         termCounter = 0
         while True:
-            fieldEndPos = stringsRow.find(FIELD_DELIMITER, rowCursor)
+            fieldEndPos = bytes_find(stringsRow, FIELD_DELIMITER, rowCursor)
             if fieldEndPos < 0: ## no delimiter after the last field:
-                fieldEndPos = len(stringsRow) + 1 
-            stringsField = stringsRow[rowCursor:fieldEndPos]
+                fieldEndPos = len(stringsRow) + 1
+            rowCursorPlus2 = rowCursor + 2
+            dictCode = stringsRow[rowCursor:rowCursorPlus2]
+            valueField = stringsRow[rowCursorPlus2:fieldEndPos]
+            dictEntry = self.dictionary.get(dictCode)
+            if not dictEntry:
+                print "Error: Unrecognized dictionary code in field '%s'" % dictCode + valueField
+                for key, value in self.dictionary.iteritems():
+                    print "DICT ENTRY", key, value
+            termType = dictEntry[0]
+            uncompressedString = dictEntry[1]
             term = self.get_ith_term(termCounter)
+            term.term_type = termType
             termCounter += 1
-            ## PHONY UNTIL GARY ADDS TO THE PROTOCOL:
-            if termCounter < 3:
-                term.term_type = StringsTerm.URI_TYPE
-                term.encoded_namespace = 'NS'
-                term.namespace = None
-                term.localname = stringsField[0:len(stringsField)]
+            if termType == URI_TYPE:
+                term.namespace = uncompressedString
+                term.localname = valueField
                 ## TEMPORARY
                 if not term.localname:
                     print "BREAK ON BUG"
                 ## END TEMPORARY 
                 term.uri = None
-            else:
-                term.term_type = StringsTerm.LITERAL_TYPE 
-                term.literal_value = stringsField[0:len(stringsField)]
+            elif termType == LITERAL_TYPE:
+                term.literal_value = valueField
+            elif termType == TYPED_LITERAL_TYPE:
+                term.datatype = uncompressedString
+                term.literal_value = valueField
+            elif termType == LANGUAGE_LITERAL_TYPE:
+                term.language = uncompressedString
+                term.literal_value = valueField
+            else:  ## must be a dictionary entry:
+                termCounter -= 1
+                self.dictionary.process_dictionary_field(termType, valueField)
             rowCursor = fieldEndPos + FIELD_DELIMITER_LENGTH
             if rowCursor >= len(stringsRow):
                 break
@@ -308,7 +365,10 @@ class ResultReader:
 import datetime
 
 def test_read_file(path):
-    file = codecs.open(path, 'r', 'utf-8')
+    print "FIELD_DELIM", ord(FIELD_DELIMITER) #, FIELD_DELIMITER    
+    print "RECORD_DELIM", ord(RECORD_DELIMITER) #, RECORD_DELIMITER
+    #file = codecs.open(path, 'r', 'utf-8')
+    file = open(path) 
     rr = ResultReader(file)
     tuples = JDBCTuples(rr)
     tupleCount = 0
@@ -326,14 +386,58 @@ def test_read_file(path):
     print "Retrieved %s tuples in time %s" % (tupleCount, elapsedTime)
 
 def test1():
-    paths = ["/Users/bmacgregor/Desktop/gary/swp-1e2.out",
-             "/Users/bmacgregor/Desktop/gary/swp-1e3.out",
-             "/Users/bmacgregor/Desktop/gary/swp-1e4.out",
-             "/Users/bmacgregor/Desktop/gary/swp-1e5.out",
-             #"/Users/bmacgregor/Desktop/gary/swp-1e6.out",
+    paths = ["/Users/bmacgregor/Desktop/gary/swp-2e2.out",
+             "/Users/bmacgregor/Desktop/gary/swp-2e3.out",
+             "/Users/bmacgregor/Desktop/gary/swp-2e4.out",
+#             "/Users/bmacgregor/Desktop/gary/swp-1e5.out",
+#             "/Users/bmacgregor/Desktop/gary/swp-2e6.out",
              ]
     for p in paths:
         test_read_file(p)
+
+def test2():
+    ## CHANGE ENCODING TO BELL, BACKUP DELIMITERS
+    for i in [2,6]:
+        input = "/Users/bmacgregor/Desktop/gary/swp-1e%i.out" % i
+        output = "/Users/bmacgregor/Desktop/gary/swp-2e%i.out" % i
+        infile = open(input)
+        outfile = open(output, 'w')
+        contents = infile.read()
+        bytes = []
+        for c in contents:
+            b = c
+            if ord(b) == ord(C1_RECORD_DELIMITER):
+                b = RECORD_DELIMITER
+            elif ord(b) == ord(C0_FIELD_DELIMITER):
+                b = FIELD_DELIMITER
+            #bytes.append(b)
+            outfile.write(b)
+#        newContents = ''.join(bytes)
+#        outfile.write(newContents)
+        outfile.flush()
+        outfile.close()
+        print "Done translating to ", output
+
+def test3():
+    ## CREATE CUT_DOWN FILES
+    input = "/Users/bmacgregor/Desktop/gary/swp-2e6.out"
+    infile = open(input)
+    contents = infile.read(810900)
+    for pair in [(3,1000),  (5, 100000)]:
+        output = "/Users/bmacgregor/Desktop/gary/swp-2e%i.out" % pair[0]
+        outfile = open(output, 'w')
+        pos = 0
+        print "Begin", len(contents)
+        for i in range(0, pair[1]):
+            nextPos = contents.find(RECORD_DELIMITER, pos)
+            #print "I", i, nextPos
+            pos = nextPos + 1
+        print "Copy to position", pos
+        outfile.write(contents[0:pos])
+        outfile.flush()
+        outfile.close()
+        print "Done creating", output
+
     
 if __name__ == '__main__':
     choices = [i for i in range(1,17)]
@@ -342,5 +446,5 @@ if __name__ == '__main__':
         print "\n==========================================================================="
         print "Test Run Number ", choice, "\n"
         if choice == 1: test1()
-#        elif choice == 2: test2()
-#        elif choice == 3: test3()
+        elif choice == 2: test2()
+        elif choice == 3: test3()
