5107c28ecf27f45b794ca9231687425befa8404b
marijnh <marijnh>
2009-03-04 09:08:31 +0000

    add planner argument to evalSparqlQuery

--- src2/franz/miniclient/repository.py
+++ src2/franz/miniclient/repository.py
@@ -60,7 +60,7 @@ class Repository:
         return jsonRequest(self.curl, "GET", self.url + "/writeable")
 
     def evalSparqlQuery(self, query, infer=False, context=None, namedContext=None, callback=None,
-                        bindings=None):
+                        bindings=None, planner=None):
         """Execute a SPARQL query. Context can be None or a list of
         contexts -- strings in "http://foo.com" form or "null" for the
         default context. Return type depends on the query type. ASK
@@ -72,7 +72,7 @@ class Repository:
             bindings = [a + " " + b for a, b in bindings.items()]
         return jsonRequest(self.curl, "GET", self.url,
                            urlenc(query=query, infer=infer, context=context, namedContext=namedContext,
-                                  environment=self.environment, bind=bindings),
+                                  environment=self.environment, bind=bindings, planner=planner),
                            rowreader=callback and RowReader(callback))
 
     def evalPrologQuery(self, query, infer=False, callback=None, limit=None):
@@ -426,6 +426,7 @@ def test2():
     rep.addStatement("\"San Francisco\"", "\"loc\"", pp(37.783333, -122.433334))
     rep.addStatement("\"Salvador\"", "\"loc\"", pp(-13.083333, -38.45))
     print [x[0] for x in rep.getStatementsHaversine(typ2, "\"loc\"", 50, 0, 1000)]
+    print [x[0] for x in rep.getStatementsInsideBox(typ2, "\"loc\"", 0.08, 0.09, 51.0, 52.0)]
 
 if __name__ == '__main__':
     choice = 2
