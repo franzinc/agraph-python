a64fa26c8495d1e88d55cc19eb22aba28e013d97
Marijn Haverbeke <marijnh@gmail.com>
2009-10-05 18:33:18 +0200

    adjust create*GeoLiteral to compensate for get*GeoType now returning a string with < and > around it

--- src2/franz/miniclient/repository.py
+++ src2/franz/miniclient/repository.py
@@ -264,7 +264,7 @@ class Repository(Service):
 
     def createCartesianGeoLiteral(self, type, x, y):
         """Create a geo-spatial literal of the given type."""
-        return "\"%+g%+g\"^^<%s>" % (x, y, type)
+        return "\"%+g%+g\"^^%s" % (x, y, type)
 
     class UnsupportedUnitError(Exception):
         def __init__(self, unit): self.unit = unit
@@ -289,7 +289,7 @@ class Repository(Service):
             return sign + (("%%0%dd" % digits) % fl) + (".%07d" % ((number - fl) * 10000000))
 
         conv = self.unitDegreeFactor(unit)
-        return "\"%s%s\"^^<%s>" % (asISO6709(lat * conv, 2), asISO6709(long * conv, 3), type)
+        return "\"%s%s\"^^%s" % (asISO6709(lat * conv, 2), asISO6709(long * conv, 3), type)
 
     def getStatementsHaversine(self, type, predicate, lat, long, radius, unit="km", limit=None):
         """Get all the triples with a given predicate whose object
