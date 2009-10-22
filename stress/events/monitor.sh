88b0ddddc2ec9c9359fcf3a04bbe6c46d4226bdb
Ross Jekel <rjekel@franz.com>
2009-10-21 18:54:57 -0700

    * Added top process to nmon monitoring

--- stress/events/monitor.sh
+++ stress/events/monitor.sh
@@ -3,7 +3,7 @@
 if [ "$1" = start ]
 then
     echo "monitor.sh: Starting monitoring for $2..."
-    nmon -C agraph -F "$2.nmon" -s 10
+    nmon -t -C agraph -F "$2.nmon" -s 10
 elif [ "$1" = end ]
 then
     echo "monitor.sh: Ending monitoring."
