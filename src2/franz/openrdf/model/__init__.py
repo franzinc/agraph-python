be348c7d2d631ebba33c6e01a2e6457ba8a8ef56
Kevin Layer <layer@franz.com>
2012-02-25 08:27:22 -0800

    Update and/or add copyright
    Change-Id: I0f7e678a1d7e27431b633c6cfd604a5e429a8409

--- src2/franz/openrdf/model/__init__.py
+++ src2/franz/openrdf/model/__init__.py
@@ -1,5 +1,13 @@
 # pylint: disable-msg=C0111
 
+###############################################################################
+# Copyright (c) 2006-2012 Franz Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+###############################################################################
+
 from __future__ import absolute_import
 
 from .literal import Literal
