661b83fc45196574023c9c6641becf48fdd159bc
Tadeusz Sznuk <ts@franz.com>
2016-11-04 14:45:16 -0700

    Make the tutorial *really* work through execfile.
    Change 6627 contained a bug and did not set the directory correctly.
    
    Change-Id: Ied0150828b2786ae3fe5aae0880dba4b8b109084
    Reviewed-on: https://gerrit.franz.com:9080/6671
    Reviewed-by: Tadeusz Sznuk <ts@franz.com>
    Tested-by: Hudson <hudson@franz.com>
    Reviewed-by: Ahmon Dancy <dancy@franz.com>
    Tested-by: Ahmon Dancy <dancy@franz.com>

--- tutorial/tutorial_examples.py
+++ tutorial/tutorial_examples.py
@@ -16,8 +16,10 @@ import os, urllib, datetime, time, sys
 
 CURRENT_DIRECTORY = os.getcwd() 
 
-# Directory containing the data files
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+# Directory containing the data files.
+# Use the location of the script file or the current working
+# directory if that location is not known.
+BASE_DIR = os.path.dirname(os.path.realpath(__file__)) if '__file__' in globals() else os.getcwd()
 
 AG_HOST = os.environ.get('AGRAPH_HOST', 'localhost')
 AG_PORT = int(os.environ.get('AGRAPH_PORT', '10035'))
