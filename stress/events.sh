6252ee2c3be7fab7563a98c0e1fa95deaa0817ec
Tadeusz Sznuk <ts@franz.com>
2016-12-15 05:49:19 -0800

    Use Nexus to get packages in stress/events.sh
    Previously the static wheelhouse on san1 was used.
    
    Change-Id: I10e690c52d762043c296e51754e5b6193ed0abad
    Reviewed-on: https://gerrit.franz.com:9080/6893
    Reviewed-by: Tadeusz Sznuk <ts@franz.com>
    Tested-by: Hudson <hudson@franz.com>
    Reviewed-by: Ahmon Dancy <dancy@franz.com>

--- stress/events.sh
+++ stress/events.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-# Prebuilt wheels location
-export WHEELHOUSE="file:///net/san1/disk1/wheelhouse/"
+# Nexus repository URL
+export NEXUS_URL="https://san1.franz.com:8443/repository/pypi-group/simple"
 
-# Use prebuilt packages ONLY by default
-export AG_PIP_OPTS="--use-wheel --find-links=${WHEELHOUSE} --no-index"
+# Certificate for the repository (RH hosts lack intermediate certs)
+export NEXUS_CERT="../nexus.ca.crt"
+
+# Pip options
+export AG_PIP_OPTS="--use-wheel --index-url=${NEXUS_URL} --cert=${NEXUS_CERT} "
 
 function absdir () {
    pushd $(dirname $1) > /dev/null
@@ -19,17 +22,19 @@ STRESS_DIR=$(absdir $0)
 ENV_DIR=${STRESS_DIR}/env
 BASE_DIR=${STRESS_DIR}/..
 
+PYTHON="${ENV_DIR}/bin/python"
+PIP="${ENV_DIR}/bin/pip"
+
 if [ ! -d "${ENV_DIR}" ]
 then
     virtualenv --no-site-packages ${ENV_DIR}
-    source ${ENV_DIR}/bin/activate
-    pip install -U setuptools pip wheel ${AG_PIP_OPTS}
-else
-    source  ${ENV_DIR}/bin/activate
+    ${PIP} install -U setuptools pip wheel ${AG_PIP_OPTS}
+    # Pip sometimes damages itself while updating, this should fix it:
+    ${PYTHON} -m pip install -U --force-reinstall pip ${AG_PIP_OPTS}
 fi
 export PYCURL_SSL_LIBRARY=nss
-pip install -r ${BASE_DIR}/requirements.txt ${AG_PIP_OPTS}
-pip install -r ${BASE_DIR}/requirements2.txt ${AG_PIP_OPTS}
-pip install -e ${BASE_DIR} ${AG_PIP_OPTS}
+${PIP} install -r ${BASE_DIR}/requirements.txt ${AG_PIP_OPTS}
+${PIP} install -r ${BASE_DIR}/requirements2.txt ${AG_PIP_OPTS}
+${PIP} install -e ${BASE_DIR} ${AG_PIP_OPTS}
 cd ${STRESS_DIR}/events
-python ./events $@
+${PYTHON} ./events $@
