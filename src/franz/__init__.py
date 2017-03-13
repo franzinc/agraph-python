52784a34729d6c2a6bcc4c5533b9efb8a4f0c45c
Tadeusz Sznuk <ts@franz.com>
2017-03-13 13:58:05 -0700

    Do not use git when building a release
    `make dist` used to perform all the required version magic
    when building a release. Unfortunately git is not available
    at that time, so the process was split into 3 steps:
    
      - Run `make prepare-release`. This will strip .dev from
        the version number, commit and push the result.
        Direct access to the repository (i.e. not through gerrit)
        is required.
      - Make the release in the usual way. `make dist` will check
        if .dev has been stripped from the version number.
      - After the release do `make post-release` to increment
        the version number. The result will again be pushed to git.
    
    If there is any chance of the release making it into the hands
    of a customer please make sure you do `make publish` before
    the third step (or you can just checkout the correct revision
    and do `make publish` later).
    
    Changed VERSION to AGVERSION in the makefile.
    
    Added a release-process.md document.
    
    Change-Id: I5c1921ba47515f2736e98363a878f64238b284fd
    Reviewed-on: https://gerrit.franz.com:9080/7430
    Reviewed-by: Tadeusz Sznuk <ts@franz.com>
    Tested-by: Hudson <hudson@franz.com>
    Reviewed-by: Ahmon Dancy <dancy@franz.com>

--- src/franz/__init__.py
+++ src/franz/__init__.py
@@ -1,7 +1,8 @@
 # This should keep the .dev suffix in general, until a release is
-# made.  The release script takes care of stripping the .dev out.
-# The first four numbers are the AG version. The fifth number is
-# incremented with each release. Note that PEP-440 specifies that
-# versions are automatically padded with zeros - that is, 
-# 6.2.1 = 6.2.1.0.0 (and also 6.2.0.dev = 6.2.0.0.dev).
+# made.  The pre-release makefile target takes care of stripping the
+# .dev out.  The post-release target increments the version and adds
+# .dev back.  The first four numbers are the AG version. The fifth
+# number is incremented with each release. Note that PEP-440 specifies
+# that versions are automatically padded with zeros - that is, 6.2.1 =
+# 6.2.1.0.0 (and also 6.2.0.dev = 6.2.0.0.dev).
 __version__ = '6.2.1.dev'
