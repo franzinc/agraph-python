9853f231aa7b5b0a7218ee5cb333de3c6555c2e1
Ahmon Dancy <dancy@franz.com>
2017-06-09 12:00:55 -0700

    Next dev version: 6.2.2.0.4.dev
    Change-Id: I529ad1757a1b54e923913f2a81b847673405bb84

--- src/franz/__init__.py
+++ src/franz/__init__.py
@@ -5,4 +5,4 @@
 # number is incremented with each release. Note that PEP-440 specifies
 # that versions are automatically padded with zeros - that is, 6.2.1 =
 # 6.2.1.0.0 (and also 6.2.0.dev = 6.2.0.0.dev).
-__version__ = '6.2.2.0.3'
+__version__ = '6.2.2.0.4.dev'
