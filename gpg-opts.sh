0a13993e34e00f2648d518c2cd9797b8fb380861
Ahmon Dancy <dancy@franz.com>
2018-11-27 14:55:45 -0800

    Fixed many bugs in gpg-opts.sh
    Show usage info if no arg is provided to the script.
    
    Fixed the logic testing AG_GPG_PASSPHRASE and AG_GPG_PASSPHRASE_FILE
    
    Fixed the test for passphraseless keys.  Prior to this it would just
    block, waiting for input on stdin.
    
    Fixed the call to 'read' which used single quotes around the
    ${GPG_PROMPT}, preventing it form being expanded.
    
    Change-Id: Idb321d055f8e6fb01370f4d24e6b4a414c457844
    Reviewed-on: https://gerrit.franz.com:9080/12286
    Tested-by: Hudson <hudson@franz.com>
    PTEST: Hudson <hudson@franz.com>
    Reviewed-by: Tadeusz Sznuk <ts@franz.com>
    Reviewed-by: Ahmon Dancy <dancy@franz.com>

--- gpg-opts.sh
+++ gpg-opts.sh
@@ -19,6 +19,11 @@ set -e
 
 PYPI_GPG_KEY=$1
 
+if [ -z "$PYPI_GPG_KEY" ]; then
+    echo Usage: $0 gpg-key-name-or-id 1>&2
+    exit 1
+fi
+
 # Prompt used when reading the passphrase from stdin:
 GPG_PROMPT="Enter GPG passphrase for ${PYPI_GPG_KEY} to sign the package:"
 
@@ -32,25 +37,25 @@ esac
 # This part is always returned (it specifies the key name)
 printf -- "-u %q " "${PYPI_GPG_KEY}"
 
-if [ -z "${AG_GPG_PASSPHRASE+x}" ]; then
+if [ -n "${AG_GPG_PASSPHRASE}" ]; then
     # Read the passphrase from a variable.
     # Note that this is insecure since the passphrase will appear
     # on command line of gpg
     printf -- "--passphrase %q" "${AG_GPG_PASSPHRASE}"
-elif [ -z "${AG_GPG_PASSPHRASE_FILE+x}" ]; then
+elif [ -n "${AG_GPG_PASSPHRASE_FILE}" ]; then
     # Passphrase saved to a file
     printf -- "--passphrase-file %q" "${AG_GPG_PASSPHRASE_FILE}"
 elif [ "${AG_NO_GPG_AGENT}" == "y" ]; then
     # We must avoid gpg-agent because the terminal can't support it
     # First check if we really need a passphrase
     if gpg --batch -o /dev/null --passphrase-fd 0 \
-           --local-user "${PYPI_GPG_KEY}" -as - 2> /dev/null; then
+           --local-user "${PYPI_GPG_KEY}" -as - 2> /dev/null </dev/null; then
         # No passphrase needed
         echo -n
     else
         # Prompt manually to avoid gpg-agent.
         # This is as insecure as using AG_GPG_PASSPHRASE
-        read -s -r -p '${GPG_PROMPT}' PASS
+        read -s -r -p "${GPG_PROMPT}" PASS
         printf -- "--passphrase %q" "${PASS}"
     fi
 else
