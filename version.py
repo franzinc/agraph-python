a727cb70608e0ddc4c881a2d76ce567df5551ccf
Tadeusz Sznuk <ts@franz.com>
2018-11-30 15:37:29 +0100

    Minor fixes in the version script
    Made it work with dev versions other than dev0 and also
    fixed a few style issues.
    
    Change-Id: Id8f3170dea1833cd47287f1b22aa11d71e050769
    Reviewed-on: https://gerrit.franz.com:9080/12316
    Reviewed-by: Tadeusz Sznuk <ts@franz.com>
    Tested-by: Hudson <hudson@franz.com>
    PTEST: Hudson <hudson@franz.com>
    Reviewed-by: Ahmon Dancy <dancy@franz.com>

--- version.py
+++ version.py
@@ -5,11 +5,11 @@
 #   set V: set the version number to V
 #   next: Increment the fifth version segment,
 #         make sure 'dev0' is at the end.
-#   undev: Strip 'dev0' from the version number.
+#   undev: Strip 'devX' from the version number.
 #   verify-dev: Exit with non-zero status if the version
-#               does not end in '.dev0'.
+#               does not end in '.devX'.
 #   verify-not-dev: Exit with non-zero status if the 
-#                   version ends in .dev0.
+#                   version ends in .devX.
 import os.path
 import re
 import sys
@@ -20,14 +20,16 @@ INIT = os.path.join(BASE_DIR, 'src', 'franz', '__init__.py')
 
 pattern = re.compile(r'__version__\s*=\s*.*$', re.M)
 
+
 def get_version():
-    "Read the version number from sources."
+    """Read the version number from sources."""
     sys.path.insert(0, os.path.join(BASE_DIR, 'src'))
     version_module = __import__('franz')
     return version_module.__version__
 
+
 def set_version(v):
-    "Change the version number to V."
+    """Change the version number to V."""
     old = get_version()
     sys.stderr.write('%s -> %s\n' % (old, v))
     with open(INIT, 'r+') as f:
@@ -37,21 +39,30 @@ def set_version(v):
         f.truncate()
         f.write(text)
 
+
 def remove_dev(v):
-    "Remove the dev0 suffix from V if it is there."
-    if v.endswith('.dev0'):
-        return v[:-5]
+    """Remove the devX suffix from V if it is there."""
+    if is_dev(v):
+        return '.'.join(v.split('.')[:-1])
     return v
 
+
 def inc(v):
-    "Increment the patch level in V."
+    """Increment the patch level in V."""
     segments = [int(s) for s in remove_dev(v).split('.')]
     # Advance the patch level
     segments[2] = segments[2] + 1
     # Construct a new version string in MAJOR.MINOR.PATCH.dev0 format
     return '.'.join(list(map(str, segments[0:3])) + ['dev0'])
-    
+
+
+def is_dev(version):
+    """Check if a given version number is a dev version."""
+    return re.match(r'^.*\.dev\d+$', version)
+
+
 def main(args):
+    """Entry point."""
     cmd = args[1] if len(args) > 1 else 'get'
     if cmd == 'get':
         sys.stdout.write(get_version())
@@ -62,16 +73,17 @@ def main(args):
     elif cmd == 'undev':
         set_version(remove_dev(get_version()))
     elif cmd == 'verify-dev':
-        if not get_version().endswith('.dev0'):
+        if not is_dev(get_version()):
             sys.stderr.write('Expected a .dev0 version number.\n')
             sys.exit(1)
     elif cmd == 'verify-not-dev':
-        if get_version().endswith('.dev0'):
-            sys.stderr.write('Expected a release (non .dev0) version number.\n')
+        if is_dev(get_version()):
+            sys.stderr.write('Expected a release (non .dev) version number.\n')
             sys.exit(1)
     else:
         sys.stderr.write('Unknown command: %s\n' % cmd)
         sys.exit(1)
-        
+
+
 if __name__ == '__main__':
     main(sys.argv)
