4f2d0a03b007b182d39ccef3ad7d0df30b3a43d1
John Foderaro <jkf@franz.com>
2022-04-11 02:26:54 -0700

    Next dev version: 101.0.10.dev0
    Change-Id: Ic3437d6a3329ae1fbf343d2cf0ee6a0069fe0f11
    Reviewed-on: https://gerrit.franz.com:9080/c/agraph-python/+/20429

--- src/franz/__init__.py
+++ src/franz/__init__.py
@@ -2,4 +2,4 @@
 # made.  The pre-release makefile target takes care of stripping the
 # .dev0 out.  The post-release target increments the version and adds
 # .dev0 back.
-__version__ = '101.0.9'
+__version__ = '101.0.10.dev0'
